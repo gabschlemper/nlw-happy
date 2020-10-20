bbf3b92c6f848f7b01daa6e9595eac68be67cac7
Gabriela Schlemper <gabschlemper@gmail.com>
2020-10-20 15:02:27 -0300

    Initial files

---
+++ frontend/src/routes.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Landing from './pages/Landing';
+
+import OrphanagesMap from './pages/OrphanagesMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
+
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/app" component={OrphanagesMap} />
+
+        <Route path="/orphanages/create" component={CreateOrphanage} />
+        <Route path="/orphanages/:id" component={Orphanage} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Routes;
